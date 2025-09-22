cb721d7da4db6d729ad407ae9777d45d179799f1
tummaadityareddy <tummaadityareddy@gmail.com>
2025-09-22 19:36:32 +0530

    introduction

---
+++ src/main/java/com/vars/varsemp/VarsempApplication.java
@@ -0,0 +1,13 @@
+package com.vars.varsemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarsempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarsempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vars/varsemp/VarsempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vars.varsemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarsempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
